plomgrading/plom-client

commit 68243b7a19de27b2c1375c5b46a585043081668a

Move ID images getter to baseMessenger

plom/client/identifier.py
@@ -413,7 +413,7 @@ class IDClient(QWidget):
             return
         # else try to grab it from server
         try:
-            imageList = messenger.IDrequestImage(test)
+            imageList = messenger.ID_request_images(test)
         except PlomSeriousException as e:
             self.throwSeriousError(e)
             return

plom/baseMessenger.py
@@ -10,6 +10,7 @@ import threading
 
 import urllib3
 import requests
+from requests_toolbelt import MultipartDecoder
 
 from plom import __version__, Plom_API_Version, Default_Port
 from plom.plom_exceptions import PlomBenignException, PlomSeriousException
@@ -492,6 +493,46 @@ class BaseMessenger:
         finally:
             self.SRmutex.release()
         return messenger_response
+
+    def request_ID_images(self, code):
+        self.SRmutex.acquire()
+        try:
+            response = self.session.get(
+                "https://{}/ID/images/{}".format(self.server, code),
+                json={"user": self.user, "token": self.token},
+                verify=False,
+            )
+            response.raise_for_status()
+            imageList = []
+            for img in MultipartDecoder.from_response(response).parts:
+                imageList.append(
+                    BytesIO(img.content).getvalue()
+                )  # pass back image as bytes
+        except requests.HTTPError as e:
+            if response.status_code == 401:
+                raise PlomAuthenticationException() from None
+            elif response.status_code == 404:
+                raise PlomSeriousException(
+                    "Cannot find image file for {}.".format(code)
+                ) from None
+            elif response.status_code == 410:
+                raise PlomBenignException(
+                    "That ID group of {} has not been scanned.".format(code)
+                ) from None
+            elif response.status_code == 409:
+                raise PlomSeriousException(
+                    "Another user has the image for {}. This should not happen".format(
+                        code
+                    )
+                ) from None
+            else:
+                raise PlomSeriousException(
+                    "Some other sort of error {}".format(e)
+                ) from None
+        finally:
+            self.SRmutex.release()
+
+        return imageList
 
     def get_annotations(self, num, question, epoch=None):
         """Download the latest annotations (or a particular set of annotations).

plom/manager/manager.py
@@ -1187,7 +1187,7 @@ class Manager(QWidget):
         test = int(self.ui.predictionTW.item(idi[0].row(), 0).text())
         sid = int(self.ui.predictionTW.item(idi[0].row(), 1).text())
         try:
-            imageList = managerMessenger.IDrequestImage(test)
+            imageList = managerMessenger.request_ID_images(test)
         except PlomException as err:
             ErrorMessage(err).exec_()
             return
@@ -1459,7 +1459,7 @@ class Manager(QWidget):
                 return
 
         test = int(self.ui.reviewIDTW.item(r, 0).text())
-        imageList = managerMessenger.IDrequestImage(test)
+        imageList = managerMessenger.request_ID_images(test)
         inames = []
         with tempfile.TemporaryDirectory() as td:
             for i in range(len(imageList)):

plom/managerMessenger.py
@@ -417,46 +417,6 @@ class ManagerMessenger(BaseMessenger):
 
         return imageList
 
-    def IDrequestImage(self, code):
-        self.SRmutex.acquire()
-        try:
-            response = self.session.get(
-                "https://{}/ID/images/{}".format(self.server, code),
-                json={"user": self.user, "token": self.token},
-                verify=False,
-            )
-            response.raise_for_status()
-            imageList = []
-            for img in MultipartDecoder.from_response(response).parts:
-                imageList.append(
-                    BytesIO(img.content).getvalue()
-                )  # pass back image as bytes
-        except requests.HTTPError as e:
-            if response.status_code == 401:
-                raise PlomAuthenticationException() from None
-            elif response.status_code == 404:
-                raise PlomSeriousException(
-                    "Cannot find image file for {}.".format(code)
-                ) from None
-            elif response.status_code == 410:
-                raise PlomBenignException(
-                    "That ID group of {} has not been scanned.".format(code)
-                ) from None
-            elif response.status_code == 409:
-                raise PlomSeriousException(
-                    "Another user has the image for {}. This should not happen".format(
-                        code
-                    )
-                ) from None
-            else:
-                raise PlomSeriousException(
-                    "Some other sort of error {}".format(e)
-                ) from None
-        finally:
-            self.SRmutex.release()
-
-        return imageList
-
     def getProgress(self, q, v):
         self.SRmutex.acquire()
         try:

plom/messenger.py
@@ -163,42 +163,6 @@ class Messenger(BaseMessenger):
 
         return idList
 
-    def IDrequestImage(self, code):
-        self.SRmutex.acquire()
-        try:
-            response = self.session.get(
-                "https://{}/ID/images/{}".format(self.server, code),
-                json={"user": self.user, "token": self.token},
-                verify=False,
-            )
-            response.raise_for_status()
-            imageList = []
-            for img in MultipartDecoder.from_response(response).parts:
-                imageList.append(
-                    BytesIO(img.content).getvalue()
-                )  # pass back image as bytes
-        except requests.HTTPError as e:
-            if response.status_code == 401:
-                raise PlomAuthenticationException() from None
-            elif response.status_code == 404:
-                raise PlomSeriousException(
-                    "Cannot find image file for {}.".format(code)
-                ) from None
-            elif response.status_code == 409:
-                raise PlomSeriousException(
-                    "Another user has the image for {}. This should not happen".format(
-                        code
-                    )
-                ) from None
-            else:
-                raise PlomSeriousException(
-                    "Some other sort of error {}".format(e)
-                ) from None
-        finally:
-            self.SRmutex.release()
-
-        return imageList
-
     # ------------------------
 
     # TODO - API needs improve. Both of these throw a put/patch to same url = /ID/tasks/{tgv}

commit 72cc5eea2d3d7655c7919b5fda071cc2563558cd

Identifier calling wrong msgr method
Typo introduced in 68243b7a19de27b2c1375c5b46a585043081668a, months ago.
Poor test coverage :(

Fixes #1708.

plom/client/identifier.py
@@ -429,7 +429,7 @@ class IDClient(QWidget):
             return
         # else try to grab it from server
         try:
-            imageList = self.msgr.ID_request_images(test)
+            imageList = self.msgr.request_ID_images(test)
         except PlomSeriousException as e:
             self.throwSeriousError(e)
             return