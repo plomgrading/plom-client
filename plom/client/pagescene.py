6236d3b56a4813a2bb4b706767a535240814545e
Colin B. Macdonald <cbm@m.fsf.org>
2025-01-21 22:02:11 -0800

    revert

--- plom/client/pagescene.py
+++ plom/client/pagescene.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2018-2022 Andrew Rechnitzer
-# Copyright (C) 2020-2025 Colin B. Macdonald
+# Copyright (C) 2020-2024 Colin B. Macdonald
 # Copyright (C) 2020 Victoria Schuster
 # Copyright (C) 2022 Joey Shi
 # Copyright (C) 2024 Aden Chan
