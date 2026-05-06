68754031e88604d3b05b935c2209117be7ac123c
Colin B. Macdonald <cbm@m.fsf.org>
2026-05-06 00:35:14 -0700

    pull in latest messenger changes

--- plomclient/messenger/base_messenger.py
+++ plomclient/messenger/base_messenger.py
@@ -74,6 +74,7 @@ Supported_Server_API_Versions = [
 # * 117
 #    - surrender task
 #    - reassign/reset return 403 not 406 for wrong user
+#    - rename /info/users to /api/beta/users
 
 
 log = logging.getLogger("messenger")
@@ -541,31 +542,43 @@ class BaseMessenger:
                     raise PlomAuthenticationException(response.reason) from None
                 raise PlomSeriousException(f"Some other sort of error {e}") from None
 
-    def get_user_list(self) -> dict[str, list[str]]:
-        """Get a list of users from the server and what groups they belong to.
+    def get_user_list(self) -> list[dict[str, Any]]:
+        """Get a list of users from the server and info such as what groups they belong to.
 
         Raises:
             PlomAuthenticationException
             PlomSeriousException: something unexpected happened.
 
         Returns:
-            Returns a dict keyed by username (string).  Each value is list
-            of strings of groups that user belongs too.
+            Returns a list of dicts, one for each user.  Keys include "uid",
+            "username", "name", and "groups".  Maybe more in the future.
+            Caution: on older servers (before 2026 summer), uid will likely
+            be blank.
         """
         if self.is_server_api_less_than(115):
             raise PlomNoServerSupportException(
-                "older server does notsupport user lists"
+                "older server does not support user lists"
             )
+
+        url = "/api/beta/users"
+        if self.is_server_api_less_than(117):
+            url = "/info/users"
 
         with self.SRmutex:
             try:
-                response = self.get_auth("/info/users")
+                response = self.get_auth(url)
                 response.raise_for_status()
-                return response.json()
+                r = response.json()
             except requests.HTTPError as e:
                 if response.status_code == 401:
                     raise PlomAuthenticationException(response.reason) from None
                 raise PlomSeriousException(f"Some other sort of error {e}") from None
+        if self.is_server_api_less_than(117):
+            r = [
+                {"uid": "", "username": u, "name": "", "groups": g}
+                for u, g in r.items()
+            ]
+        return r
 
     def requestAndSaveToken(
         self, user: str, pw: str, *, exclusive: bool = False
